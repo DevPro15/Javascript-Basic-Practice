63a39552cb678c876941d8fba08c407c5772b374
Zair15 <zairabbas1533@outlook.com>
2022-10-18 14:14:36 +0500

    Ways of console & diff between parameters and arguments

---
+++ myapp.js
@@ -0,0 +1,42 @@
+/* Ways of Console */
+
+// Mostly use for debugging and checking the data
+
+console.log("hello world");
+
+console.log(200);
+
+console.log(true);
+
+console.log(null);
+
+var num = 10;
+
+console.log(num);
+
+console.log("string", 10.11, true, false, null, num);
+
+/* ----------------------------------------------------------------------------------*/
+
+/* Parameters & Arguments */
+
+// Parameters
+
+function makeCoffee(sugar, milk) {
+  var instructions = "Boil Water";
+
+  instructions += " pour into cup,";
+
+  instructions += " add coffee granules,";
+
+  instructions += " add " + sugar + " spoon  of sugar, ";
+
+  instructions += " add" + milk + "% milk.";
+
+  return instructions;
+}
+
+// Arguments
+console.log(makeCoffee(2, 20));
+
+/* ----------------------------------------------------------------------------------*/
